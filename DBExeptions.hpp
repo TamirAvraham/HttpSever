30d35f2cd591f1298e228e93729966398e366391
tamir avraham <tamir.avrah@gmail.com>
2023-04-17 21:55:18 +0300

    added more exceptions in DbExceptions.hpp

--- DBExeptions.hpp
+++ DBExeptions.hpp
@@ -1,5 +1,12 @@
 #pragma once
 #include <stdexcept>
+#define CreateException(newClassName,InherClassName) class newClassName: InherClassName{\
+    newClassName(const std::string& message):InherClassName(message) {} \
+    newClassName(const char* message):InherClassName(message){}\
+    const char* what() const noexcept override { \
+        return InherClassName::what(); \
+    } \
+};
 
 namespace DocDB {
     class DbException : public std::runtime_error {
@@ -11,13 +18,19 @@ namespace DocDB {
         }
     };
 
-    class NotFoundError : public DbException {
+    class NotFoundException : public DbException {
     public:
-        NotFoundError(const std::string& message) : DbException(message) {}
-        NotFoundError(const char* message) : DbException(message) {}
+        NotFoundException(const std::string& message) : DbException(message) {}
+        NotFoundException(const char* message) : DbException(message) {}
         const char* what() const noexcept override {
             return DbException::what();
         }
     };
-	
+    namespace doc {
+        class DocumentNotFoundException : NotFoundException {
+            DocumentNotFoundException(const std::string& message) :NotFoundException(message) {} DocumentNotFoundException(const char* message) :NotFoundException(message) {} const char* what() const noexcept override {
+                return NotFoundException::what();
+            }
+        };
+    }
 }
