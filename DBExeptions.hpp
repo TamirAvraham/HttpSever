77f43336f52ac38a3d0bbf97cff32d547a90c759
tamir avraham <tamir.avrah@gmail.com>
2023-04-17 19:35:56 +0300

    added some errors to DBExceptions

--- DBExeptions.hpp
+++ DBExeptions.hpp
@@ -2,7 +2,22 @@
 #include <stdexcept>
 
 namespace DocDB {
-	using DBExeption = std::exception;
-	class NotFound :DBExeption {};
+    class DbException : public std::runtime_error {
+    public:
+        DbException(const std::string& message) : std::runtime_error(message) {}
+        DbException(const char* message) : std::runtime_error(message) {}
+        const char* what() const noexcept override {
+            return std::runtime_error::what();
+        }
+    };
+
+    class NotFoundError : public DbException {
+    public:
+        NotFoundError(const std::string& message) : DbException(message) {}
+        NotFoundError(const char* message) : DbException(message) {}
+        const char* what() const noexcept override {
+            return DbException::what();
+        }
+    };
 	
 }
